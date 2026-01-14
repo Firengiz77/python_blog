a20f82914bf1434583c330f742df8212c534e4bd
Firengiz77 <firengizsariyeva77@gmail.com>
2026-01-14 22:18:26 +0400

    blog & slug

--- blogs/admin.py
+++ blogs/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 
-from blogs.models import Category
+from blogs.models import Blog, Category
+
+class BlogAdmin(admin.ModelAdmin):
+    prepopulated_fields= { 'slug': ('title',) }
+
 
 admin.site.register(Category)
+admin.site.register(Blog,BlogAdmin)
--- blogs/models.py
+++ blogs/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Category(models.Model):
@@ -13,3 +14,28 @@ class Category(models.Model):
     # admin panelde sutunun adini deyisdirmek ucundu
     def __str__(self):
         return self.name    
+
+
+
+STATUS_CHOISES = (
+    (0, 'Deactive'),
+    (1, 'Active')
+)
+
+
+class Blog(models.Model):
+    title = models.CharField(max_length=100)
+    slug = models.SlugField(max_length=150, unique=True, blank=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to="uploads/blog/%Y/%d/%m")
+    description = models.TextField(max_length=2000)
+    status = models.IntegerField(choices=STATUS_CHOISES, default=0)
+    is_feature = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title    
+
+
